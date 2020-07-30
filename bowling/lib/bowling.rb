84566a7234068f254340c018b33c7d8be20baea1
Eduardo PL <mini0n.000@gmail.com>
2020-07-30 13:02:41 -0500

    testy thingy

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -4,14 +4,27 @@ require 'byebug'
 require 'awesome_print'
 require 'colorize'
 
+require './bowl_errors'
+require './bowl_interface'
 require './bowl_files'
 
 puts 'testing bowling...'
 
-archivo = '/home/mini0n/Desktop/jobsity/sample1.txt'
+module Bowling
+  test_1 = '/home/mini0n/Desktop/jobsity/sample1.txt'
+  test_2 = '/home/mini0n/Desktop/jobsity/sample2.txt'
+  test_3 = '/home/mini0n/Desktop/jobsity/sample3.txt'
 
-test = BowlFiles.new(archivo)
+  test = BowlFiles.new(test_3)
+  ap test.parse_bowling_game
 
-byebug
+  test = BowlFiles.new(test_2)
+  ap test.parse_bowling_game
 
-puts 'end'
+  test = BowlFiles.new(test_1)
+  ap test.parse_bowling_game
+
+  # byebug
+
+  puts 'end'
+end
