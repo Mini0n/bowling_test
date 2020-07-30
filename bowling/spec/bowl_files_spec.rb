271bd56fc894f506cc9ff6479951ba329d6cc07b
Eduardo PL <mini0n.000@gmail.com>
2020-07-30 12:49:48 -0500

    added code to test

--- bowling/spec/bowl_files_spec.rb
+++ bowling/spec/bowl_files_spec.rb
@@ -1,6 +1,31 @@
 # frozen_string_literal: true
 
-require 'bowl_files'
+require 'spec_helper'
+
+# require 'bowl_files'
 
 RSpec.describe 'BowlFiles' do
+  context '#load_file' do
+    it 'algo' do
+      puts 'Bowlfiles'
+    end
+  end
+
+  context '#parse_bowling_game' do
+  end
+
+  context '#valid_throws?' do
+  end
+
+  context '#hash_by_player' do
+  end
+
+  context '#valid_value?' do
+  end
+
+  context '#valid_bowled?' do
+  end
+
+  context '#valid_file_rows?' do
+  end
 end
