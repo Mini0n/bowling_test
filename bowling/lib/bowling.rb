67a66431d9e4cbd8b7b406eab5cf7dafb6cccec4
Eduardo PL <mini0n.000@gmail.com>
2020-07-31 01:51:18 -0500

    testy update

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -7,22 +7,43 @@ require 'colorize'
 require './bowl_errors'
 require './bowl_interface'
 require './bowl_files'
+require './bowl_game'
+require './bowl_printer'
 
-puts 'testing bowling...'
+puts 'testing bowling...'.yellow
 
 module Bowling
   test_1 = '/home/mini0n/Desktop/jobsity/sample1.txt'
   test_2 = '/home/mini0n/Desktop/jobsity/sample2.txt'
   test_3 = '/home/mini0n/Desktop/jobsity/sample3.txt'
+  test_4 = '/home/mini0n/Desktop/jobsity/sample4.txt'
+  test_5 = '/home/mini0n/Desktop/jobsity/sample5.txt'
+  test_6 = '/home/mini0n/Desktop/jobsity/sample6.txt'
 
-  test = BowlFiles.new(test_3)
-  ap test.parse_bowling_game
+  # test = BowlFiles.new(test_3)
+  # ap test.parse_bowling_game
 
-  test = BowlFiles.new(test_2)
-  ap test.parse_bowling_game
+  # test = BowlFiles.new(test_2)
+  # ap test.parse_bowling_game
 
-  test = BowlFiles.new(test_1)
-  ap test.parse_bowling_game
+  files = BowlFiles.new(test_6)
+  file_game = files.parse_bowling_game
+
+  puts 'loaded game'.yellow
+  ap file_game
+
+  puts 'solving game...'.yellow
+  game = BowlGame.new(file_game)
+  solv = game.score_game
+
+  puts 'solved game'.yellow
+  ap solv
+
+  puts 'print scoreboard...'.yellow
+  screen = BowlPrinter.new(solv)
+  screen.print_game
+
+  byebug
 
   # byebug
 
